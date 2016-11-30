74749fb7c498e1b700a8760fecbe16943cd83a85
cotix <n.goldsmid@student.utwente.nl>
2016-11-30 17:55:16 +0100

    Update transtable

--- transpositions.c
+++ transpositions.c
@@ -14,15 +14,16 @@ void initTable(unsigned long long size) {
 }
 
 inline void save(int score, int work, int flag, int s, int idx) {
-    table[idx].slot[s].board[0] = board[0];
-    table[idx].slot[s].board[1] = board[1];
+    table[idx].slot[s].board[0] = board[0]&getPotential(0);
+    table[idx].slot[s].board[1] = board[1]&getPotential(1);
+    table[idx].slot[s].board[2] = board[1]|board[0];
     table[idx].slot[s].data = flag | (score<<2);
     table[idx].slot[s].work = work;
 
 }
 
 inline void putTable(int score, int work, int flag) {
-    unsigned long long idx = hash%transSize;
+    unsigned long long idx = getHash()%transSize;
     if (table[idx].slot[0].data == 0) {
         save(score, work, flag, 0, idx);
     } else if (table[idx].slot[1].data == 0) {
@@ -33,24 +34,34 @@ inline void putTable(int score, int work, int flag) {
 }
 
 inline int getType() {
-    unsigned long long idx = hash%transSize;
-    if (table[idx].slot[0].board[0] == board[0] &&
-        table[idx].slot[0].board[1] == board[1]) {
+    unsigned long long idx = getHash()%transSize;
+    unsigned long long p1 = board[0]&getPotential(0);
+    unsigned long long p2 = board[1]&getPotential(1);
+    if (table[idx].slot[0].board[0] == p1 &&
+        table[idx].slot[0].board[1] == p2 &&
+        table[idx].slot[0].board[2] == board[0]|board[1]) {
         return table[idx].slot[0].data&TYPE_MASK;
-    } else if (table[idx].slot[1].board[0] == board[0] &&
-               table[idx].slot[1].board[1] == board[1]) {
+    }
+    else if (table[idx].slot[1].board[0] == p1 &&
+             table[idx].slot[1].board[1] == p2 &&
+             table[idx].slot[1].board[2] == board[0]|board[1]) {
         return table[idx].slot[1].data&TYPE_MASK;
     }
     return TT_EMPTY;
 }
 
 inline int getScore() {
-    unsigned long long idx = hash%transSize;
-    if (table[idx].slot[0].board[0] == board[0] &&
-        table[idx].slot[0].board[1] == board[1]) {
+    unsigned long long idx = getHash()%transSize;
+    unsigned long long p1 = board[0]&getPotential(0);
+    unsigned long long p2 = board[1]&getPotential(1);
+    if (table[idx].slot[0].board[0] == p1 &&
+        table[idx].slot[0].board[1] == p2 &&
+        table[idx].slot[0].board[2] == board[0]|board[1]) {
         return table[idx].slot[0].data&RESULT_MASK;
-    } else if (table[idx].slot[1].board[0] == board[0] &&
-               table[idx].slot[1].board[1] == board[1]) {
+    }
+    else if (table[idx].slot[1].board[0] == p1 &&
+             table[idx].slot[1].board[1] == p2 &&
+             table[idx].slot[1].board[2] == board[0]|board[1]) {
         return table[idx].slot[1].data&RESULT_MASK;
     }
     return 0;
