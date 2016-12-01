50ddf658ebc8089dd49b5b3754ca2e288f554ae7
cotix <n.goldsmid@student.utwente.nl>
2016-12-01 13:39:46 +0100

    Actually use the upto date negamax function

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define INFINITY 100000
-#define FULLSEARCH 0
+#define FULLSEARCH 1
 unsigned long long workCounter = 0;
-int order[] = {5,6,9,10,12,15,3,0,8,4,1,2,7,11,14,13};
+int order[] = {5,6,9,10,0,3,12,15,8,4,1,2,7,11,14,13};
 
 int alphabeta(int depth, int alpha, int beta, int color) {
     unsigned long long oldWork = workCounter++;
@@ -21,14 +21,27 @@ int alphabeta(int depth, int alpha, int beta, int color) {
         }        
     }
     int oldAlpha = alpha;
-    if (hasWon(!color)) return -INFINITY-depth;
+    if (hasWon(!color)) return -INFINITY;
     if (depth == 0 || isFull()) return 0;
 
-/*    for (int idx = 0; idx != 16; ++idx) {
+    for (int idx = 0; idx != 16; ++idx) {
         if (!move(idx, color)) continue;
-        if (hasWon(color)) return INFINITY;
-        unmove(idx,color);
-    }*/
+        if (hasWon(color)) {
+            unmove(idx, color);
+            return INFINITY;
+        }
+        unmove(idx, color);
+        if (!move(idx, !color)) continue;
+        if (hasWon(!color)) {
+            unmove(idx,!color);
+            move(idx, color);
+            int res = -alphabeta(depth-1, -beta, -alpha, !color);
+            unmove(idx, color);
+            return res;
+        }
+        unmove(idx, !color);
+    }
+
     int best = -INFINITY;
     for (int idx = 0; idx != 16; ++idx) {
         int i = order[idx];
@@ -45,7 +58,7 @@ int alphabeta(int depth, int alpha, int beta, int color) {
     unsigned long long work = workCounter - oldWork;
     unsigned long long logWork = 0;
     while ((work>>=1) > 0) logWork++;
-    if (FULLSEARCH || best != 0) putTable(best, logWork, f);
+    if (best != 0 || FULLSEARCH != 0) putTable(best, logWork, f);
     return best;
 }
 
@@ -54,26 +67,8 @@ int alphabeta(int depth, int alpha, int beta, int color) {
 
 int main(int argc, char **argv) {
     initZobrist();
-    initTable(123456791);
+    initTable(311299913);
     initBoard();
-    while (1) {
-        int best = -INFINITY;
-        int m = -1;
-        for (int idx = 0; idx != 16; ++idx) {
-            if (!move(idx, 0)) continue;
-            int res = -alphabeta(8, -INFINITY, INFINITY, 1);
-            if (res >= best) {
-                best = res;
-                m = idx;
-            }
-            unmove(idx,0);
-        }
-        move(m, 0);
-        printf("%i %i\n", m, best);
-        if (hasWon(0)) puts("Ik win!");
-        scanf("%i", &m);
-        move(m, 1);
-        if (hasWon(1)) puts("Wow, goed gespeeld!");
-    }
-
+    printf("%i\n", alphabeta(64, -INFINITY, INFINITY, 0));
+    return 0;
 }
